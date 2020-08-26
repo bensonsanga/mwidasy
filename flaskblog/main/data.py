94ca77a2c06c17b27b67d1855a2b0facf8723d1b
Benson Sanga <bensonsanga@hotmail.com>
2020-08-26 11:14:08 +0300

    iniial commit

---
+++ flaskblog/main/data.py
@@ -0,0 +1,176 @@
+# Articles for entourage below
+articles = [
+    {
+        'author': 'Benson Sanga',
+        'img': 'https://scontent.fbud2-1.fna.fbcdn.net/v/t1.0-1/p160x160/25659525_1660669194003997_812779653662270213_n.jpg?_nc_cat=102&_nc_ht=scontent.fbud2-1.fna&oh=d25af60e7c89bf71c5add09550701c2c&oe=5CE6E9FF',
+        'title': 'Introduction to Rhino 6 Grasshopper in 2019',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018',
+        'type': 'Video',
+        'tags': 'Grasshopper',
+        'link': '#'
+    },
+    {
+        'author': 'Benson Sanga',
+        'img': 'https://scontent.fbud2-1.fna.fbcdn.net/v/t1.0-1/p160x160/25659525_1660669194003997_812779653662270213_n.jpg?_nc_cat=102&_nc_ht=scontent.fbud2-1.fna&oh=d25af60e7c89bf71c5add09550701c2c&oe=5CE6E9FF',
+        'title': 'Introduction to Rhino 6 Grasshopper in 2019',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018',
+        'type': 'Video',
+        'tags': 'Grasshopper',
+        'link': '#'
+    },
+    {
+        'author': 'Benson Sanga',
+        'img': 'https://scontent.fbud2-1.fna.fbcdn.net/v/t1.0-1/p160x160/25659525_1660669194003997_812779653662270213_n.jpg?_nc_cat=102&_nc_ht=scontent.fbud2-1.fna&oh=d25af60e7c89bf71c5add09550701c2c&oe=5CE6E9FF',
+        'title': 'Introduction to Rhino 6 Grasshopper in 2019',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018',
+        'type': 'Video',
+        'tags': 'Grasshopper',
+        'link': '#'
+    },
+    {
+        'author': 'Benson Sanga',
+        'img': 'https://scontent.fbud2-1.fna.fbcdn.net/v/t1.0-1/p160x160/25659525_1660669194003997_812779653662270213_n.jpg?_nc_cat=102&_nc_ht=scontent.fbud2-1.fna&oh=d25af60e7c89bf71c5add09550701c2c&oe=5CE6E9FF',
+        'title': 'Introduction to Rhino 6 Grasshopper in 2019',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018',
+        'type': 'Video',
+        'tags': 'Grasshopper',
+        'link': '#'
+    },
+    {
+        'author': 'Benson Sanga',
+        'img': 'https://scontent.fbud2-1.fna.fbcdn.net/v/t1.0-1/p160x160/25659525_1660669194003997_812779653662270213_n.jpg?_nc_cat=102&_nc_ht=scontent.fbud2-1.fna&oh=d25af60e7c89bf71c5add09550701c2c&oe=5CE6E9FF',
+        'title': 'Introduction to Rhino 6 Grasshopper in 2019',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018',
+        'type': 'Video',
+        'tags': 'Grasshopper',
+        'link': '#'
+    },
+    {
+        'author': 'Benson Sanga',
+        'img': 'https://scontent.fbud2-1.fna.fbcdn.net/v/t1.0-1/p160x160/25659525_1660669194003997_812779653662270213_n.jpg?_nc_cat=102&_nc_ht=scontent.fbud2-1.fna&oh=d25af60e7c89bf71c5add09550701c2c&oe=5CE6E9FF',
+        'title': 'Introduction to Rhino 6 Grasshopper in 2019',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018',
+        'type': 'Video',
+        'tags': 'Grasshopper',
+        'link': '#'
+    },
+    {
+        'author': 'Benson Sanga',
+        'img': 'https://scontent.fbud2-1.fna.fbcdn.net/v/t1.0-1/p160x160/25659525_1660669194003997_812779653662270213_n.jpg?_nc_cat=102&_nc_ht=scontent.fbud2-1.fna&oh=d25af60e7c89bf71c5add09550701c2c&oe=5CE6E9FF',
+        'title': 'Introduction to Rhino 6 Grasshopper in 2019',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018',
+        'type': 'Video',
+        'tags': 'Grasshopper',
+        'link': '#'
+    },
+    {
+        'author': 'Benson Sanga',
+        'img': 'https://scontent.fbud2-1.fna.fbcdn.net/v/t1.0-1/p160x160/25659525_1660669194003997_812779653662270213_n.jpg?_nc_cat=102&_nc_ht=scontent.fbud2-1.fna&oh=d25af60e7c89bf71c5add09550701c2c&oe=5CE6E9FF',
+        'title': 'Introduction to Rhino 6 Grasshopper in 2019',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018',
+        'type': 'Video',
+        'tags': 'Grasshopper',
+        'link': '#'
+    }
+]
+
+
+# Video turorials data here
+videos = [
+    {
+        'author': 'Benson Sanga',
+        'img': 'https://i.ytimg.com/vi/pxNrUg7t0Mg/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBSKMkNKAnPCjbNKxeIYIiq0tunIQ',
+        'title': 'Introduction to Rhino 6 Grasshopper in 2019',
+        'content': 'First post content',
+        'time': '43:07',
+        'shortDescription': 'This is video is about architecture presentations amd how you can use data to visulize different design decisions and come up with interesting design off the bat',
+        'link': 'Grasshopper',
+        'summary': 'Lets take a look at how one can change the normal background of plans to a more interesting one',
+        'number': '#001'
+    },
+    {
+        'author': 'Benson Sanga',
+        'img': 'https://i.ytimg.com/vi/pxNrUg7t0Mg/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBSKMkNKAnPCjbNKxeIYIiq0tunIQ',
+        'title': 'Introduction to Rhino 6 Grasshopper in 2019',
+        'content': 'First post content',
+        'time': '43:07',
+        'shortDescription': 'This is video is about architecture presentations amd how you can use data to visulize different design decisions and come up with interesting design off the bat',
+        'link': 'Grasshopper',
+        'summary': 'Lets take a look at how one can change the normal background of plans to a more interesting one',
+        'number': '#001'
+    },
+    {
+        'author': 'Benson Sanga',
+        'img': 'https://i.ytimg.com/vi/pxNrUg7t0Mg/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBSKMkNKAnPCjbNKxeIYIiq0tunIQ',
+        'title': 'Introduction to Rhino 6 Grasshopper in 2019',
+        'content': 'First post content',
+        'time': '43:07',
+        'shortDescription': 'This is video is about architecture presentations amd how you can use data to visulize different design decisions and come up with interesting design off the bat',
+        'link': 'Grasshopper',
+        'summary': 'Lets take a look at how one can change the normal background of plans to a more interesting one',
+        'number': '#001'
+    },
+    {
+        'author': 'Benson Sanga',
+        'img': 'https://i.ytimg.com/vi/pxNrUg7t0Mg/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBSKMkNKAnPCjbNKxeIYIiq0tunIQ',
+        'title': 'Introduction to Rhino 6 Grasshopper in 2019',
+        'content': 'First post content',
+        'time': '43:07',
+        'shortDescription': 'This is video is about architecture presentations amd how you can use data to visulize different design decisions and come up with interesting design off the bat',
+        'link': 'Grasshopper',
+        'summary': 'Lets take a look at how one can change the normal background of plans to a more interesting one',
+        'number': '#001'
+    },
+    {
+        'author': 'Benson Sanga',
+        'img': 'https://i.ytimg.com/vi/pxNrUg7t0Mg/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBSKMkNKAnPCjbNKxeIYIiq0tunIQ',
+        'title': 'Introduction to Rhino 6 Grasshopper in 2019',
+        'content': 'First post content',
+        'time': '43:07',
+        'shortDescription': 'This is video is about architecture presentations amd how you can use data to visulize different design decisions and come up with interesting design off the bat',
+        'link': 'Grasshopper',
+        'summary': 'Lets take a look at how one can change the normal background of plans to a more interesting one',
+        'number': '#001'
+    }
+]
+#Data for gh scripts
+gh_scripts = [
+    {   
+        'proj_id' : 1,
+        'name1': 'Cube geometry',
+        'name2': 'Cube Tessalation',
+        'name3': 'Computational',
+        'name4': 'Uniform sized',
+        'imgs': ['1.png', '2.png', '3.png', '4.png'],
+        'title': 'Cube geomtery',
+        'content': 'First post content',
+        'type': 'Grasshopper',
+        'file': 'Gh',
+        'skill': 'beginner',
+        'summary': 'Lets take a look at how one can change the normal background of plans to a more interesting one',
+        'number': '#001'
+    },
+    {
+        'proj_id' : 2,
+        'name': 'Benson_Sanga1',
+        'author': 'Benson Sanga',
+        'imgs': ['1.png', '2.png', '3.png'],
+        'title': 'Introduction to Rhino 6 Grasshopper in 2019',
+        'content': 'First post content',
+        'time': '43:07',
+        'shortDescription': 'This is video is about architecture presentations amd how you can use data to visulize different design decisions and come up with interesting design off the bat',
+        'type': 'Grasshopper',
+        'file': 'Gh',
+        'skill': 'beginner',
+        'summary': 'Lets take a look at how one can change the normal background of plans to a more interesting one',
+        'number': '#002'
+    }
+]
---
+++ flaskblog/main/routes.py
@@ -0,0 +1,138 @@
+from flask import render_template, request, Blueprint
+from flaskblog.models import Post
+from .data import *
+
+main = Blueprint('main', __name__)
+
+
+@main.route("/")
+@main.route("/home")
+def home():
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
+    return render_template('home.html', posts=posts)
+
+
+@main.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+
+@main.route("/entourage")
+def entourage():
+    return render_template('entourage.html', title='Entourage', articles=articles)
+
+
+@main.route("/new_article")
+def new_article():
+    return render_template('new_article.html', title='New Article')
+
+
+@main.route("/tutorials")
+def tutorials():
+    return render_template('tutorials.html', title='Tutorials', videos=videos)
+
+
+@main.route("/tutorial001")
+def tutorial001():
+    return render_template('001.html', title='Tutorial001')
+
+
+@main.route("/new_tutorials")
+def new_tutorials():
+    return render_template('new_tutorials.html', title='New Tutorials')
+
+
+@main.route("/dwg")
+def dwg():
+    return render_template('dwg.html', title='Dwg')
+
+
+@main.route("/new_dwg")
+def new_dwg():
+    return render_template('new_dwg.html', title='New File')
+
+
+@main.route("/scripts")
+def scripts():
+    return render_template('scripts.html', title='Scripts')
+
+
+@main.route("/models")
+def models():
+    return render_template('models.html', title='Models')
+
+
+@main.route("/search")
+def search():
+    return render_template('search.html', title='Search')
+
+
+@main.route("/search1")
+def search1():
+    return render_template('search1.html', title='Search1')
+
+
+@main.route("/search2")
+def search2():
+    return render_template('search2.html', title='Search2')
+
+
+@main.route("/icons")
+def icons():
+    return render_template('icons.html', title='Projects')
+
+
+@main.route("/new_project")
+def new_project():
+    return render_template('new_project.html', title='New Project')
+
+
+@main.route("/maps")
+def maps():
+    return render_template('maps.html', title='Maps')
+
+
+@main.route("/map1")
+def map1():
+    return render_template('map.html', title='Brzezin')
+
+
+@main.route("/map2")
+def map2():
+    return render_template('map2.html', title='lodz botanic garden')
+
+
+@main.route("/map3")
+def map3():
+    return render_template('map3.html', title='lodz Piotrkowska Street')
+
+
+@main.route("/sketchmap")
+def sketchmap():
+    return render_template('sketchmap.html', title='Map Sketch')
+
+
+@main.route("/sketch3d")
+def sketch3d():
+    return render_template('sketch3d.html', title='3D Map Sketch')
+
+
+@main.route("/map01")
+def map01():
+    return render_template('arcgis/map01.html', title='Map 3D')
+
+
+@main.route("/map4")
+def map4():
+    return render_template('map4.html', title='Piotrkowska Street')
+
+
+@main.route("/map5")
+def map5():
+    return render_template('map5.html', title='Lodz Map of Murals')
+
+@main.route("/projectshow/<int:proj_id1>")
+def projectshow(proj_id1):
+    project = next((x for x in gh_scripts if x["proj_id"] == proj_id1), None)
+    return render_template('projectshow.html', title='projectshow', project=project)
